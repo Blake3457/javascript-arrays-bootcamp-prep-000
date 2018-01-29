74418fcdd7cc5d978e56ef809c46dac72ba14e73
Blake Kunishige <blake3457@yahoo.com>
2018-01-29 05:54:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,5 +26,5 @@ function accessElementInArray(array, index){
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  return chocolateBars.shift()
+  return chocolateBars.unshift()
 }
