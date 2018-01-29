6470aac81807ed4fb909996db47b3f40a6c32aa8
Blake Kunishige <blake3457@yahoo.com>
2018-01-29 04:49:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,8 @@ var ingredient6 = "garlic"
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(a, e) {
- 
+ a.unshift
+ return a
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   
