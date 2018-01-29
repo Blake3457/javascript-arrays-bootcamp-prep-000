89af35f81bcc868c69f6001e411dccc8becedc1f
Blake Kunishige <blake3457@yahoo.com>
2018-01-29 05:02:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 var candyString = "foo"
 
 function addElementToBeginningOfArray(chocolateBars, candyString) {
-
+return [candyString, ...chocolateBars]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   
