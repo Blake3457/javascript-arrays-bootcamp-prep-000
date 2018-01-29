28ec4a5dc6ffde6e1fe5a1f4f2f177de9b07a076
Blake Kunishige <blake3457@yahoo.com>
2018-01-29 05:10:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
 function addElementToEndOfArray(chocolateBars, candyString){
   return [...chocolateBars, candyString]
 }
-function destructivelyAddElementToEndOfArray(){
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
   chocolateBars.push(candyString)
   return chocolateBars
 }
